518518006c65996f8a8febf748b9e8c436c40e64
GeonHyeong5036 <48436020+GeonHyeong5036@users.noreply.github.com>
2019-05-08 21:10:25 +0900

    Create Operation add
    insert DB(sss), user data add operation

---
+++ PHP/ServerSetting-sss/includes/Constants.php
@@ -0,0 +1,10 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'super');
+    define('DB_NAME', 'sss');
+
+    define('USER_CREATED', 101);
+    define('USER_EXISTS', 102);
+    define('USER_FAILURE', 103);
+?>
